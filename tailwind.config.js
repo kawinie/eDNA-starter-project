bdc3dbef56daa70f9088031b09f0dfadebaa943d
Kawin <pechetrk@oregonstate.edu>
2020-11-27 16:28:08 +0700

    Minor changes to config files and package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
 	future: {
 		// removeDeprecatedGapUtilities: true,
@@ -8,6 +10,9 @@ module.exports = {
 	},
 	theme: {
 		extend: {},
+		colors: {
+			gray: colors.blueGray,
+		},
 	},
 	variants: {},
 	plugins: [],
